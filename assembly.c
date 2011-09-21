3bccde953d228de238a2017d1cb095724226d54e
Philip Flarsheim <op@mindbane-minion.localdomain>
2011-09-21 10:29:44 -0400

    Reading input chars, added main to .gitignore, now echo's characters back to user.

--- assembly.c
+++ assembly.c
@@ -9,20 +9,26 @@ void __asm_initialize__() {
 }
 
 void __asm_putch__() {
-	asm("movem.l %a0-%a1/%d1-%d2,-(%a7)");
-	asm("move.l #0x8001,%a0");
-	asm("L1: move.b (%a0),%d1");
-	asm("btst #0,%d1");
-	asm("beq.s L3");
-	asm("L2: move.b (%a0),%d2");
+	asm("movem.l %a0-%a1/%d1-%d2,-(%a7)");	//Save working registers
+	asm("move.l #0x8001,%a0");				//Get address of ACIA in A0
+	asm("L1: move.b (%a0),%d1");			//Read ACIA status
+	asm("btst #0,%d1");						//Test RDRF bit
+	asm("beq.s L3");						
+	asm("L2: move.b (%a0),%d2");			
 	asm("btst #0,%d2");
 	asm("beq L2");
-	asm("L3: btst #1,%d1");
-	asm("beq L1");
-	asm("move.b %d0,2(%a0)");
-	asm("movem.l (%a7)+,%a0-%a1/%d1-%d2");
+	asm("L3: btst #1,%d1");					//Repeat
+	asm("beq L1");							//until ACIA Tx ready
+	asm("move.b %d0,2(%a0)");				//Transmit output
+	asm("movem.l (%a7)+,%a0-%a1/%d1-%d2");	//Restore working registers
 }
 
 void __asm_getch__() {
-	/* TODO: write */
+	asm("movem.l %d1/%a1,-(%a7)");	//Save working registers
+	asm("move.l #0x8001,%a1");		//Get address of ACIA in A1
+	asm("CON_1: move.b (%a1),%d1");	//Read ACIA status 
+	asm("btst #0,%d1");				//Test RDRF 
+	asm("beq CON_1");				//Repeat until RDRF true 
+	asm("move.b 2(%a1),%d0");		//Read input from ACIA 
+	asm("movem.l (%a7)+,%a1/%d1");	//Restore working registers 
 }
