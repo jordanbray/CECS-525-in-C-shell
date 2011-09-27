f5a48403ad37a8133d665424ab86aea327959426
Op" Philip Flarsheim <mindbane@gmail.com>
2011-09-27 08:41:06 -0400

    Moved test functions into its own command library.

---
+++ commands/test.c
@@ -0,0 +1,62 @@
+#include "test.h"
+
+//Function to test our implementation of malloc
+void *test_malloc(int bytes) {
+	void *ptr = kmalloc(bytes);
+	putstr("Malloc'd and got: ");
+	puthexint((int)ptr);
+	putch('\n');
+	return ptr;
+}
+
+//Function to test our implementation of free
+void test_free(void *ptr) {
+	putstr("Freeing: ");
+	puthexint((int)ptr);
+	putch('\n');
+	kfree(ptr);
+}
+
+void memtest() {
+	void *p1 = test_malloc(124);
+	void *p2 = test_malloc(124);
+	void *p3 = test_malloc(124);
+    test_free(p1);
+    test_free(p2);
+    test_free(p3);
+}
+
+void test_tree() {
+    struct tnode *root = NULL;
+    root = tnode_insert(root, "one", ((void*)1));
+    root = tnode_insert(root, "two", ((void*)2));
+    root = tnode_insert(root, "three", ((void*)3));
+    root = tnode_insert(root, "four", ((void*)4));
+    root = tnode_insert(root, "five", ((void*)5));
+    root = tnode_insert(root, "negative 1", (void*)-1);
+    struct tnode *node = tnode_search(root, "four");
+    if (((int *)node->value) == ((int *)4))
+        putstr("SUCCESS\n");
+    else
+        putstr("FAILURE\n");
+    tnode_destroy(root);
+
+}
+
+int test(int argc, const char** argv)
+{
+	int i;
+	for (i=1; i < argc; i++)
+	{
+		if (strcmp("mem", argv[i]) == 0)
+		{
+			putstr("\n****Testing Memory****\n");
+			memtest();
+		}
+		else if(strcmp("tree", argv[i]) == 0)
+		{
+			putstr("\n****Testing Binary Tree****\n");
+			test_tree();
+		}
+	}
+}
--- main.c
+++ main.c
@@ -19,49 +19,6 @@
 #include "commands/commands.h"
 #include "auth/authentication.h"
 
-//Function to test our implementation of malloc
-void *test_malloc(int bytes) {
-	void *ptr = kmalloc(bytes);
-	putstr("Malloc'd and got: ");
-	puthexint((int)ptr);
-	putch('\n');
-	return ptr;
-}
-
-//Function to test our implementation of free
-void test_free(void *ptr) {
-	putstr("Freeing: ");
-	puthexint((int)ptr);
-	putch('\n');
-	kfree(ptr);
-}
-
-void memtest() {
-	void *p1 = test_malloc(124);
-	void *p2 = test_malloc(124);
-	void *p3 = test_malloc(124);
-    test_free(p1);
-    test_free(p2);
-    test_free(p3);
-}
-
-void test_tree() {
-    struct tnode *root = NULL;
-    root = tnode_insert(root, "one", ((void*)1));
-    root = tnode_insert(root, "two", ((void*)2));
-    root = tnode_insert(root, "three", ((void*)3));
-    root = tnode_insert(root, "four", ((void*)4));
-    root = tnode_insert(root, "five", ((void*)5));
-    root = tnode_insert(root, "negative 1", (void*)-1);
-    struct tnode *node = tnode_search(root, "four");
-    if (((int *)node->value) == ((int *)4))
-        putstr("SUCCESS\n");
-    else
-        putstr("FAILURE\n");
-    tnode_destroy(root);
-
-}
-
 //Main kernel function
 void main() {
 	//Must initialize the ACIA
@@ -93,9 +50,12 @@ void main() {
 			putstr("Password: ");
 			getpass(pass, 30);
 		} while (checkLogin(user, pass) != 1);
-
+		
 		//Begin the shell
 		shell(user);
+		
+		kfree(user);
+		kfree(pass);
 	}
 		
 }
