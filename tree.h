14f5b6b6aab863f3185f9c7b607abf6664f55312
Jordan Bray <jordanbray@gmail.com>
2011-09-26 20:20:30 -0400

    Missing some const's

--- tree.h
+++ tree.h
@@ -12,12 +12,12 @@ struct tnode {
 };
 
 /* insert, swap, search value, search minimum and search maximum values */
-struct tnode *tnode_insert(struct tnode *p, char *key, void *value);
+struct tnode *tnode_insert(struct tnode *p, const char *key, void *value);
 struct tnode *tnode_swap(struct tnode *p);
-struct tnode *tnode_search(struct tnode *p, char *key);
+struct tnode *tnode_search(struct tnode *p, const char *key);
 struct tnode *tnode_searchmin(struct tnode *p);
 struct tnode *tnode_searchmax(struct tnode *p);
-struct tnode *tnode_startswith(struct tnode *p, char *key);
+struct tnode *tnode_startswith(struct tnode *p, const char *key);
 /* destroy, count tree nodes */
 void tnode_destroy(struct tnode *p);
 int tnode_count(struct tnode *p);
