60fe0b670a284ac2ffb0d572ecc65d1910580ecf
giga1699 <giga1699@gmail.com>
2011-12-10 22:33:33 -0500

    Added some updates to string.h/string.c

--- string.h
+++ string.h
@@ -1,6 +1,8 @@
 #ifndef __STRING_H__
 #define __STRING_H__
 
+typedef unsigned long size_t;
+
 #include "kmem.h"
 
 
@@ -12,15 +14,31 @@ void *memcpy(void *, const void *, int);
 void *memset(void *, int, int);
 int memcmp(const void *, const void *, int);
 void *memmove(void *, const void *, int);
+void *memchr(void *, int, size_t);
+const void *memchr(const void *, int, size_t);
 int strcmp(const char *, const char *);
 int strncmp(const char *, const char *, int);
 char *strcpy(char *, const char *);
+char *strncpy(char *, const char *, size_t);
+char *strcat(char *, const char *);
+char *strncat(char *, const char *, size_t);
 int strstartswith(const char *, const char *);
 char *match_beginning(const char *, const char *);
 int strtoint(const char *);
 
 int chars_matched(const char *s1, const char *s2);
 #define IS_PRINTABLE(ch) ((ch) >= ' ' && (ch) <= '~')
+
+//TODO
+/*
+ * strcspn
+ * strpbrk
+ * strrchr
+ * strspn
+ * strtok
+ * strerr
+ * <size_t>
+ */
 
 #endif
 
